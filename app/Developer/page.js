f2f2d7ed2279ed5d4cdef17027fe5aae664717bc
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2026-03-31 19:49:10 +0500

    done for now

--- app/Developer/page.js
+++ app/Developer/page.js
@@ -7,6 +7,7 @@ import SearchBar from "../components/DeveloperComponents/SearchBar";
 import AllDevelopers from "../components/DeveloperComponents/AllDevelopers";
 import WebDevelopers from "../components/DeveloperComponents/WebDevelopers";
 import FamousDevelopers from "../components/DeveloperComponents/FamousDevelopers";
+import Newsletter from "../components/newsletter";
 
 const Developer = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -19,6 +20,7 @@ const Developer = () => {
       <AllDevelopers searchQuery={searchQuery} />
       <WebDevelopers />
       <FamousDevelopers />
+      <Newsletter/>
     </>
   );
 };
---
+++ app/components/newsletter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Newsletter = () =>{
+    return(
+        <>
+        <section className='mt-20 py-10'>
+            <div className='bg-gradient-to-br from-[#1e1e1e] to-[#161616] w-full h-auto flex justify-center items-center p-6'>
+            <header>
+                <h1>Find Your Next Developer — Every Week</h1>
+            </header>
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default Newsletter;
