85a0721fe30e18f9d0ad92db2a1e76151c8afb33
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2026-04-12 18:31:35 +0500

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.optimization = {
-      ...config.optimization,
-      moduleIds: 'deterministic',
-      runtimeChunk: {
-        name: 'runtime',
-      },
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          react: {
-            name: 'react',
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            priority: 10,
-            reuseExistingChunk: true,
-            enforce: true,
-          },
-        },
-      },
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
-export default nextConfig;
+module.exports = nextConfig;
